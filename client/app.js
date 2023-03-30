387f1160510b9234d10374c1c7dc0f5928d21999
John Kim <jkim005@yahoo.com>
2023-03-30 17:19:13 -0500

    connect client and server

--- client/app.js
+++ client/app.js
@@ -64,7 +64,29 @@ const handleSubmit = async (e) => {
 
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
-}
+
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
+};
 
 form.addEventListener("submit", handleSubmit);
 form.addEventListener("keyup", (e) => {
