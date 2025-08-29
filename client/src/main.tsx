daed4852f8780d7411b4332614396c69074f1750
John Kim <jkdev220@gmail.com>
2025-08-29 07:00:53 -0500

    fix: wrap app with ThemeProvider to resolve useTheme errors

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from './providers/ThemeProvider';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
 );
